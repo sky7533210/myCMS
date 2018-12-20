c4e8a65a94758b665adf1befeac7cfd85b0ec076
sky7533210 <877385083@qq.com>
2018-12-20 22:04:57 +0800

    test添加了文章管理的删除按钮

--- admin/manager.php
+++ admin/manager.php
@@ -10,7 +10,7 @@
 </ul>
 <div id="articlebox">
     <table border="1px">
-        <tr><td><input type="text"><a href="javascript:void(0)">搜索</a></td><td>文章管理</td></tr>
+        <tr><td></td><td><input type="text"></td><td><a href="javascript:void(0)">搜索</a></td></tr>
         <tr><td><input type="checkbox">全选</td><td>文章标题</td><td>作者</td><td>类别</td><td>发布时间</td><td>操作</td></tr>
     </table>
 </div>
@@ -35,7 +35,8 @@
                             var table=$("#articlebox table");
                             for(var i in data){
                                 table.append("<tr id='"+data[i]['aid']+"'><td><input type='checkbox'></td><td>"+data[i]['title']+
-                                    "</td><td>"+data[i]["username"]+"</td><td>"+data[i]["classname"]+"</td><td>"+data[i]["date"]+"</td></tr>");
+                                    "</td><td>"+data[i]["username"]+"</td><td>"+data[i]["classname"]+"</td><td>"+data[i]["date"]+
+                                    "</td><td><a href='javascript:void(0)'>删除</a></td></tr>");
                             }
                         }
                     });
